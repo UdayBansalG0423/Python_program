d6cb96ffea0fd294f2c57280e08e9316037b71bc
Uday Bansal <142798403+UdayBansalG0423@users.noreply.github.com>
2023-12-02 01:11:43 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,11 @@
+print("hello World")
+
+
+#types of data stored in a variable
+str="hi every one"  # it store only string 
+int=53 # it store numeric values but not decimals
+float=52.1 # it store decimal numeric values
+boolean=True # it have only true or False
+
+
+print(str,int,float,boolean)
